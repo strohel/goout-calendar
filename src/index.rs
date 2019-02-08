f971b3d16acb69465fcc97b31122d515b317975a
Matěj Laitl <matej@laitl.cz>
2019-02-08 15:16:21 +0100

    index: add Bulma CSS style

--- src/index.rs
+++ src/index.rs
@@ -5,17 +5,24 @@ const INDEX_HTML: &str = r#"
     <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
         <title>GoOut Calendar Exporter</title>
     </head>
     <body>
-        <h1>GoOut Calendar Exporter</h1>
-        <p>
-            This is a simple proxy Web service that serves events of a given
-            user of <a href="https://goout.net/">GoOut.net</a> in an iCalendar
-            format. See
-            <a href="https://github.com/strohel/goout-calendar">homepage
-            on GitHub</a> for more info and source code.
-        </p>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">GoOut Calendar Exporter</h1>
+            <p class="subtitle">
+                This is a simple proxy Web service that serves events of a given
+                user of <a href="https://goout.net/">GoOut.net</a> in an iCalendar
+                format. See
+                <a href="https://github.com/strohel/goout-calendar">homepage
+                on GitHub</a> for more info and source code.
+            </p>
+        </div>
+    </section>
     </body>
     </html>
 "#;
