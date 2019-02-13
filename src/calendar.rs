964dbca7c2bd7b8720ace0189dcb84c4fd175ef0
Matěj Laitl <matej@laitl.cz>
2019-02-13 23:32:16 +0100

    Switch to legacy HTML-snippet-serving endpoint
    This is silly, see introduced comment.

--- src/calendar.rs
+++ src/calendar.rs
@@ -3,14 +3,19 @@ use rocket::get;
 use serde_json::{map::Map, value::Value};
 use std::error::Error;
 
-const ENDPOINT_URL: &str = "https://goout.net/services/feeder/v1/events.json";
+// This is a legacy endpoint that returns HTML snippets, which are silly to
+// parse. There is /services/feeder/v1/events.json with nice JSON output, BUT
+// I didn't manage to make it to display events for a particular user
+// (maybe even a different endpoint should be used).
+const ENDPOINT_URL: &str = "https://goout.net/legacy/follow/calendarFor";
 
 #[get("/services/feeder/usercalendar.ics?<id>")]
 pub(in crate) fn serve(id: u64) -> Result<String, Box<dyn Error>> {
     let client = Client::new();
     let params = &[
-        ("source", "strohel.eu"),
-        ("user", &id.to_string()), // TODO: seems not to actually select user?
+        ("userId", id.to_string()),
+        ("future", "false".to_string()),
+        ("page", 1.to_string()),
     ];
 
     let mut response = client
@@ -20,13 +25,12 @@ pub(in crate) fn serve(id: u64) -> Result<String, Box<dyn Error>> {
         .error_for_status()?;
     eprintln!("Retrieved {}.", response.url());
 
-    let mut results: Vec<String> = Vec::new();
     let json = goout_response_json(&mut response)?;
+    let html_val = json.get("html").ok_or("No html key in response.")?;
+    let html_str = html_val.as_str().ok_or("Key html is not a string.")?;
 
-    if let Some(schedule) = json.get("schedule") {
-        results.push(parse_schedule_json(schedule)?);
-    }
-
+    let mut results = Vec::new();
+    results.push(html_str);
     Ok(results.join("\n"))
 }
 
@@ -41,21 +45,4 @@ fn goout_response_json(response: &mut Response) -> Result<Map<String, Value>, Bo
         return Err(format!("Expected message OK, got {}.", message).into());
     }
     Ok(json)
-}
-
-fn parse_schedule_json(schedule: &Value) -> Result<String, Box<dyn Error>> {
-    let vec = schedule
-        .as_array()
-        .ok_or("Value schedule is not an array.")?;
-
-    let mut urls: Vec<&str> = Vec::new();
-    let message = format!("Got {} schedule items:", vec.len());
-    urls.push(&message);
-
-    for one_schedule in vec {
-        let value = one_schedule.get("url").ok_or("No url in one schedule")?;
-        urls.push(value.as_str().ok_or("url is not string")?);
-    }
-    urls.push("");
-    Ok(urls.join("\n"))
 }
