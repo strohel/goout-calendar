02f7b002dfa338cf73f022024a11672ace8c68af
Matěj Laitl <matej@laitl.cz>
2019-02-14 00:57:19 +0100

    calendar: parse HTML response

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,6 +1,9 @@
+use html5ever::tree_builder::QuirksMode;
 use reqwest::{Client, Response};
 use rocket::get;
+use scraper::{Html, Selector};
 use serde_json::{map::Map, value::Value};
+use single::Single;
 use std::error::Error;
 
 // This is a legacy endpoint that returns HTML snippets, which are silly to
@@ -29,9 +32,7 @@ pub(in crate) fn serve(id: u64) -> Result<String, Box<dyn Error>> {
     let html_val = json.get("html").ok_or("No html key in response.")?;
     let html_str = html_val.as_str().ok_or("Key html is not a string.")?;
 
-    let mut results = Vec::new();
-    results.push(html_str);
-    Ok(results.join("\n"))
+    parse_events_html(html_str)
 }
 
 fn goout_response_json(response: &mut Response) -> Result<Map<String, Value>, Box<dyn Error>> {
@@ -45,4 +46,30 @@ fn goout_response_json(response: &mut Response) -> Result<Map<String, Value>, Bo
         return Err(format!("Expected message OK, got {}.", message).into());
     }
     Ok(json)
+}
+
+fn parse_events_html(html: &str) -> Result<String, Box<dyn Error>> {
+    // unwrap() because it would be programmer error for this not to parse.
+    let event_in_fragment_sel = Selector::parse(".eventCard .info").unwrap();
+    let name_in_event_sel = Selector::parse(".name").unwrap();
+
+    let fragment = Html::parse_fragment(html);
+    if fragment.errors.len() != 0 {
+        return Err(fragment.errors.join("\n").into());
+    }
+    if fragment.quirks_mode != QuirksMode::NoQuirks {
+        return Err(format!("HTML parsed only with quirks {:?}.", fragment.quirks_mode).into());
+    }
+
+    let mut results = Vec::new();
+    for elem in fragment.select(&event_in_fragment_sel) {
+        // map_err because following compiler message:
+        // > the trait `std::error::Error` is not implemented for `single::Error`
+        let name_elem = elem
+            .select(&name_in_event_sel)
+            .single()
+            .map_err(|_| "Zero or multiple name elements.")?;
+        results.push(name_elem.inner_html());
+    }
+    Ok(results.join("\n"))
 }
