1576424839f056df13a0cc2911ffbcdd7ff7b278
Matěj Laitl <matej@laitl.cz>
2019-02-26 18:44:11 +0100

    calendar: switch to JSON GoOut API
    I've finally discovered how to use it to my liking, yay!

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,35 +1,10 @@
 use crate::goout_api;
-use icalendar::{self, Calendar, Component};
+use icalendar::{Calendar};
 use reqwest::Client;
 use rocket::get;
 use std::error::Error;
 
 pub(in crate) type HandlerResult<T> = Result<T, Box<dyn Error>>;
-
-#[derive(Debug)]
-pub(in crate) struct Event {
-    pub name: String,
-    pub start_time: String,
-    pub end_time: Option<String>,
-    pub venue: String,
-    pub street_address: Option<String>,
-    pub address_locality: String,
-}
-
-impl Event {
-    fn to_icalendar(self: &Event) -> HandlerResult<icalendar::Event> {
-        eprintln!("Parsing '{:?}'...", self);
-        let mut ical_event = icalendar::Event::new();
-        ical_event.summary(&self.name);
-//         ical_event.starts(self.start_time);
-//         if let Some(end_time) = self.end_time {
-//             ical_event.ends(end_time);
-//         }
-        // TODO: venue, address...
-        eprintln!("Parsed as\n{}", ical_event.to_string());
-        Ok(ical_event)
-    }
-}
 
 #[get("/services/feeder/usercalendar.ics?<id>")]
 pub(in crate) fn serve(id: u64) -> HandlerResult<String> {
@@ -39,17 +14,16 @@ pub(in crate) fn serve(id: u64) -> HandlerResult<String> {
     // instead we load all pages first and only then start replying. We can
     // afford this, because the calendar endpoint would be typically called
     // infrequently and in non-interactive manner. Advantage is that we can
-    // properly report errors on HTTP level, and siplicity. Disadvantage is
+    // properly report errors on HTTP level, and simplicity. Disadvantage is
     // high latency of first byte served.
     let mut calendar = Calendar::new();
     for page in 1.. {
-        let json = goout_api::fetch_page(&client, id, page)?;
-        let (html_str, has_next) = goout_api::parse_json_reply(&json)?;
-        for event in goout_api::parse_events_html(html_str)? {
-            calendar.push(event.to_icalendar()?);
+        let events_response = goout_api::fetch_page(&client, id, page)?;
+        for event in goout_api::generate_events(&events_response)? {
+            calendar.push(event);
         }
 
-        if !has_next {
+        if !events_response.has_next {
             break;
         }
     }
--- src/goout_api.rs
+++ src/goout_api.rs
@@ -1,163 +1,71 @@
-use crate::calendar::{Event, HandlerResult};
-use html5ever::tree_builder::QuirksMode;
-use reqwest::{Client, Response};
-use scraper::{ElementRef, Html, Selector};
-use serde_json::{map::Map, value::Value};
-use single::{self, Single};
+use crate::calendar::{HandlerResult};
+use icalendar::{Component, Event};
+use reqwest::Client;
+use serde::Deserialize;
 
-// This is a legacy endpoint that returns HTML snippets, which are silly to
-// parse. There is /services/feeder/v1/events.json with nice JSON output, BUT
-// I didn't manage to make it to display events for a particular user
-// (maybe even a different endpoint should be used).
-const ENDPOINT_URL: &str = "https://goout.net/legacy/follow/calendarFor";
+const ENDPOINT_URL: &str = "https://goout.net/services/feeder/v1/events.json";
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Schedule {
+    event_id: u64,
+    url: String,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(in crate) struct EventsResponse {
+    status: u16,
+    message: String,
+    // use defaults for all other fields as error responses don't have them filled in and we want
+    // better error message than failure to parse all keys from JSON.
+    #[serde(default)]
+    pub has_next: bool,
+    #[serde(default)]
+    schedule: Vec<Schedule>,
+}
+
+impl EventsResponse {
+    fn error_for_status(self: &Self) -> HandlerResult<()> {
+        if self.message != "OK" {
+            return Err(format!("Expected message OK, got {}.", self.message).into());
+        }
+        if self.status != 200 {
+            return Err(format!("Expected status 200, got {}.", self.status).into());
+        }
+        Ok(())
+    }
+}
 
 pub(in crate) fn fetch_page(
     client: &Client,
     id: u64,
-    page: u16,
-) -> HandlerResult<Map<String, Value>> {
+    page: u8,
+) -> HandlerResult<EventsResponse> {
     let params = &[
-        ("userId", id.to_string()),
-        ("future", "false".to_string()),
-        ("page", page.to_string()),
+        ("tag", "liked"),
+        ("user", &id.to_string()),
+        ("page", &page.to_string()),
+        ("source", "strohel.eu"),
     ];
 
-    let mut response = client
+    let mut raw_response = client
         .get(ENDPOINT_URL)
         .query(params)
         .send()?
         .error_for_status()?;
-    eprintln!("Retrieved {}.", response.url());
-
-    goout_response_json(&mut response)
+    eprintln!("Retrieved {}.", raw_response.url());
+    let response: EventsResponse = raw_response.json()?;
+    response.error_for_status()?;
+    Ok(response)
 }
 
-fn goout_response_json(response: &mut Response) -> HandlerResult<Map<String, Value>> {
-    let json: Map<_, _> = response.json()?;
-    let status = json.get("status").ok_or("No status in response.")?;
-    let message = json.get("message").ok_or("No message in response.")?;
-    if status != 200 {
-        return Err(format!("Expected status 200, got {}.", status).into());
-    }
-    if message != "OK" {
-        return Err(format!("Expected message OK, got {}.", message).into());
-    }
-    Ok(json)
-}
-
-pub(in crate) fn parse_json_reply(json: &Map<String, Value>) -> HandlerResult<(&str, bool)> {
-    let html_str = json
-        .get("html")
-        .ok_or("No html key in response.")?
-        .as_str()
-        .ok_or("Key html is not a string.")?;
-    let has_next = json
-        .get("hasNext")
-        .ok_or("No hasNext key in response.")?
-        .as_bool()
-        .ok_or("Key hasNext is not a bool")?;
-    Ok((html_str, has_next))
-}
-
-pub(in crate) fn parse_events_html(html: &str) -> HandlerResult<Vec<Event>> {
-    // See calendarForReplyExample.html file of what we need to parse.
-
-    // unwrap() because it would be programmer error for this not to parse.
-    let event_in_fragment_sel = Selector::parse(fragment_selectors::EVENT).unwrap();
-
-    let fragment = Html::parse_fragment(html);
-    if !fragment.errors.is_empty() {
-        return Err(fragment.errors.join("\n").into());
-    }
-    if fragment.quirks_mode != QuirksMode::NoQuirks {
-        return Err(format!("HTML parsed only with quirks {:?}.", fragment.quirks_mode).into());
-    }
-
-    let mut events = Vec::new();
-    for elem in fragment.select(&event_in_fragment_sel) {
-        let event = Event::from_html(&elem)?;
+pub(in crate) fn generate_events(response: &EventsResponse) -> HandlerResult<Vec<Event>> {
+    let mut events: Vec<Event> = Vec::new();
+    for schedule in response.schedule.iter() {
+        let mut event = Event::new();
+        event.summary(&format!("{}: {}", schedule.event_id, schedule.url));
         events.push(event);
     }
     Ok(events)
-}
-
-mod fragment_selectors {
-    pub const EVENT: &str = ".eventCard .info";
-}
-
-mod event_selectors {
-    pub const NAME: &str = ".name[itemprop=name]";
-    pub const START_TIME: &str = "[itemprop=startDate]";
-    pub const END_TIME: &str = "[itemprop=endDate]";
-    // pub const DESCRIPTION: &str = "[itemprop=description]"; TODO: not in API reply
-    pub const VENUE: &str = "[itemprop=location] [itemprop=name]";
-    pub const STREET_ADDRESS: &str = "[itemprop=streetAddress]";
-    pub const ADDRESS_LOCALITY: &str = "[itemprop=addressLocality]";
-    // TODO: type of event, first text of <div class="timestamp">
-}
-
-impl Event {
-    fn from_html(parent: &ElementRef) -> HandlerResult<Self> {
-        let select_one_optional = |selector_str| {
-            // TODO: it would be better not to do this every time, but how?
-            let selector = Selector::parse(selector_str).unwrap();
-            let element_result = parent.select(&selector).single();
-            match element_result {
-                Ok(element) => Ok(Some(element)),
-                Err(single::Error::NoElements) => Ok(None),
-                Err(single::Error::MultipleElements) => Err(format!(
-                    "Multiple elements matching {} in {}.",
-                    selector_str,
-                    parent.html()
-                )),
-            }
-        };
-        let select_one = |selector_str| {
-            select_one_optional(selector_str)?.ok_or_else(|| {
-                format!(
-                    "No elements matching {} in {}.",
-                    selector_str,
-                    parent.html()
-                )
-            })
-        };
-
-        use event_selectors::*;
-        let name = extract_text(&select_one(NAME)?);
-        let start_time = extract_attr(&select_one(START_TIME)?, "datetime")?;
-        let end_time = select_one_optional(END_TIME)?
-            .map(|ref e| extract_attr(e, "content"))
-            .transpose()?;
-        let venue = extract_text(&select_one(VENUE)?);
-        let street_address = select_one_optional(STREET_ADDRESS)?
-            .map(|ref e| extract_attr(e, "content"))
-            .transpose()?;
-        let address_locality = extract_attr(&select_one(ADDRESS_LOCALITY)?, "content")?;
-
-        Ok(Self {
-            name,
-            start_time,
-            end_time,
-            venue,
-            street_address,
-            address_locality,
-        })
-    }
-}
-
-fn extract_text(element: &ElementRef) -> String {
-    element
-        .text()
-        .collect::<Vec<_>>()
-        .join("")
-        .trim()
-        .to_string()
-}
-
-fn extract_attr(element: &ElementRef, name: &str) -> HandlerResult<String> {
-    let value = element
-        .value()
-        .attr(name)
-        .ok_or_else(|| format!("No attribute {} in element.", name))?;
-    Ok(value.to_string())
 }
